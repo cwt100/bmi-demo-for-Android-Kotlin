09e0791515400b91b7f2107b13ce03fe58c53b2c
WanTing Cheng <cwt0000d@gmail.com>
2022-01-17 17:13:49 +0800

    Dialog to show BMI

--- app/src/main/java/com/cwt/bmi/MainActivity.kt
+++ app/src/main/java/com/cwt/bmi/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.cwt.bmi
 
+import android.app.Activity
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -33,6 +38,33 @@ class MainActivity : AppCompatActivity() {
         val bmi = weight / (height * height)
 
         Log.d("MainActivity", "BMI: ${bmi}")
-        Toast.makeText(this, bmi.toString(), Toast.LENGTH_SHORT).show()
+        //Toast.makeText(this, bmi.toString(), Toast.LENGTH_SHORT).show()
+
+        hideKeyboard()
+
+        showAlertBMI(bmi)
+    }
+
+    private fun showAlertBMI(bmi: Float) {
+
+        AlertDialog.Builder(this)
+            .setMessage("Your BMI is ${bmi}")
+            .setTitle("BMI")
+            .setPositiveButton("OK", null)
+            .setNeutralButton("Cancel", null)
+            .show()
+    }
+
+    fun Fragment.hideKeyboard() {
+        view?.let { activity?.hideKeyboard(it) }
+    }
+
+    fun Activity.hideKeyboard() {
+        hideKeyboard(currentFocus ?: View(this))
+    }
+
+    fun Context.hideKeyboard(view: View) {
+        val imm = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
